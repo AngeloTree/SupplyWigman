5d19c1211c1537d68a45fb0b20f8c6a2d34541fb
Angelo Chaknis <angelopalmtree@gmail.com>
2023-04-02 14:53:07 -0700

    update: headerShown is false

--- ProjectSupplyWigman/App.js
+++ ProjectSupplyWigman/App.js
@@ -11,8 +11,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
