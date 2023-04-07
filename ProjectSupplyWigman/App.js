60bf1e5411c8c7838e5d3870933b07080be017b8
Angelo Chaknis <angelopalmtree@gmail.com>
2023-04-06 17:37:37 -0700

    add react-navigation bottomTab

--- ProjectSupplyWigman/App.js
+++ ProjectSupplyWigman/App.js
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StatusBar } from "expo-status-bar";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
@@ -9,6 +10,7 @@ import SignIn from "./screens/SignIn";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [signIn, setSignedIn] = useState(true);
   return (
     <NavigationContainer>
       <Stack.Navigator>
