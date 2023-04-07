3e8b17d97eb8c5296440c0890d86ebe517ae69d0
Angelo Chaknis <angelopalmtree@gmail.com>
2023-04-06 18:21:19 -0700

    add tab icons

--- ProjectSupplyWigman/App.js
+++ ProjectSupplyWigman/App.js
@@ -2,25 +2,52 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import SettingIcon from "react-native-vector-icons/AntDesign";
 import { StatusBar } from "expo-status-bar";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import SignIn from "./screens/SignIn";
 import Home from "./screens/signedIn/Home";
-import Profile from "./screens/signedIn/Profile";
+import Settings from "./screens/signedIn/Settings";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [isSignedIn, setSignedIn] = useState(false);
+  const [isSignedIn, setSignedIn] = useState(true);
 
   return (
     <NavigationContainer>
       {isSignedIn ? (
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Navigator
+          initialRouteName="Feed"
+          screenOptions={{
+            tabBarActiveTintColor: "#f2d241",
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              headerShown: false,
+              tabBarLabel: "Settings",
+              tabBarIcon: ({ color, size }) => (
+                <SettingIcon name="setting" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       ) : (
         <Stack.Navigator>
