6418611c87c173707f4e461ecc819e23716b190a
Angelo Chaknis <angelopalmtree@gmail.com>
2023-04-18 22:16:57 -0700

    add FormDelete file

---
+++ ProjectSupplyWingman/components/FormDelete.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Button, Overlay, Icon } from "@rneui/themed";
+
+const FormDelete = () => {
+  return <Text>FormDelete</Text>;
+};
+
+export default FormDelete;
